0b21d43fb40da7292c42c014439672ab2e11b47c
zhuxingchong <zhuxingchong@baidu.com>
2026-06-28 09:31:55 +0800

    移除 moss_sfx_venv 虚拟环境（含超大文件 mlx.metallib，不纳入版本控制）

--- moss_sfx_venv/lib/python3.9/site-packages/mlx/include/mlx/backend/cpu/simd/type.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "mlx/backend/cpu/simd/base_simd.h"
-
-#ifdef MLX_USE_ACCELERATE
-#include "mlx/backend/cpu/simd/accelerate_simd.h"
-#endif
--- moss_sfx_venv/lib/python3.9/site-packages/mlx_lm/_version.py
+++
@@ -1,3 +0,0 @@
-# Copyright © 2023-2025 Apple Inc.
-
-__version__ = "0.29.1"
